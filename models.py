4f3d795db1a09c917d7073557272bad8a7833efa
Vendredimatin <2860957268@qq.com>
2021-04-16 11:22:44 +0800

    自己实现的Resnet

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from torchvision import models
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 def model_A(num_classes):
     model_resnet = models.resnet18(pretrained=False)
@@ -10,7 +10,8 @@ def model_A(num_classes):
 
 def model_B(num_classes):
     ## your code here
-    my_model = Net()
+    #my_model = Net()
+    my_model = ResNet(BasicBlock, [2,2,2,2])
 
     return my_model
 
@@ -78,3 +79,64 @@ class AttentionLayer(nn.Module):
         output = self.avg_pool(input).view(batch, channel)
         output = self.fc(output).view(batch, channel, 1, 1)
         return input * output.expand_as(input)
+
+# resnet
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_features, out_features, stride = 1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_features, out_features, kernel_size=3, stride=stride=, padding=1, bias=False)
+        # 待处理数据的通道数
+        self.bn1 = nn.BatchNorm2d(out_features)
+        self.conv2 = nn.Conv2d(out_features,out_features,kernel_size=3,stride=1,padding=1,bias=False)
+        self.bn2 = nn.BatchNorm2d(out_features)
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_features != self.expansion * out_features:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_features,self.expansion*out_features,1,stride=stride,padding=0,bias=False),
+                nn.BatchNorm2d(self.expansion * out_features)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes = 10):
+        super(ResNet, self).__init__()
+
+        self.in_features = 64
+        self.conv1 = nn.Conv2d(3, self.in_features, kernel_size=3, stride=1, padding=1,bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_features)
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    
+    def _make_layer(self,block,features, num_blocks,stride):
+        strides = [stride] + [1] * (num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_features, frozenset, stride))
+            self.in_features = features * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
